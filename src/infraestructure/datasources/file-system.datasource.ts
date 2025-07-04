b5288e68e26eef5bad325f2c01dee65b383ecfbd
Daniel galvan <danielricardogt@gmail.com>
2025-07-03 22:08:03 -0500

    :wrench: clean code repository and entity

--- src/infraestructure/datasources/file-system.datasource.ts
+++ src/infraestructure/datasources/file-system.datasource.ts
@@ -20,15 +20,40 @@ export class FileSystemDataSource implements LogDataSource {
     [this.allLogsPath, this.mediumLogsPath, this.highLogsPath].forEach(
       (path) => {
         if (fs.existsSync(path)) return;
-        fs.writeFileSync(path, "");
+        fs.writeFileSync(path, " ");
       }
     );
   };
 
-  saveLog(log: LogEntity): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
-  getLog(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
-    throw new Error("Method not implemented.");
+  async saveLog(newLog: LogEntity): Promise<void> {
+    const logAsJson = `${JSON.stringify(newLog)}/n`;
+    fs.appendFileSync(this.allLogsPath, logAsJson);
+    if (newLog.level === LogSeverityLevel.low) return;
+    if (newLog.level === LogSeverityLevel.medium) {
+      fs.appendFileSync(this.mediumLogsPath, logAsJson);
+    } else {
+      fs.appendFileSync(this.highLogsPath, logAsJson);
+    }
+  };
+
+
+  private readonly getLogsFromFile = (path: string): LogEntity[]=>{
+    const content = fs.readFileSync(path, "utf-8");
+    const logs = content.split('\n').map(log => LogEntity.fromJson(log));
+    return logs;
+  } 
+
+
+  async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+    switch (severityLevel) {
+      case LogSeverityLevel.low:
+        return this.getLogsFromFile(this.allLogsPath)
+      case LogSeverityLevel.medium:
+        return this.getLogsFromFile(this.mediumLogsPath);
+      case LogSeverityLevel.high:
+        return this.getLogsFromFile(this.highLogsPath);
+      default:
+        throw new Error(`${severityLevel} not implemented`);
+    }
   }
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,9 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDataSource } from "../infraestructure/datasources/file-system.datasource";
+import { LogPepositoyImpl } from "../infraestructure/repository/log/log.repository,impl";
 import { CronService } from "./cron/cron-service";
 
+const fileSystemLogRepository = new LogPepositoyImpl((new FileSystemDataSource));
 
 export class Server{
     public static start () {
@@ -11,6 +14,7 @@ export class Server{
             ()=>{
                 const url = 'https://google.com'
                new CheckService(
+                fileSystemLogRepository,
                 () => console.log('success ' + url + ' is OK'),
                 (error) => console.log(error)
                ).execute(url);
