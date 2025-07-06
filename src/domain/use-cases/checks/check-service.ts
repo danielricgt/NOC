61b586d4a197ddbce6babb9a2dafc926b65ab793
Daniel galvan <danielricardogt@gmail.com>
2025-07-05 22:31:25 -0500

    :wrench: clean code and enviromental variables

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -5,8 +5,8 @@ interface CheckServiceUseCase {
     execute(url: string): Promise<boolean> 
 }
 
-type SucessCallback = () => void;
-type FailCallback = (error: string) => void;
+type SucessCallback = (() => void) | undefined;
+type FailCallback = ((error: string) => void) | undefined;
 export class CheckService implements CheckServiceUseCase {
 
     constructor(
@@ -25,13 +25,13 @@ export class CheckService implements CheckServiceUseCase {
             }
             const newLogEntity = new LogEntity(`Service ${url} working`, LogSeverityLevel.low )
             this.logRepository.saveLog(newLogEntity);
-            this.sucessCallBack();
+            this.sucessCallBack && this.sucessCallBack();
             return true;
         } catch (error) {
             const errorMessage = `${url } is not ok ${error}`
             const log = new LogEntity(errorMessage, LogSeverityLevel.high);
             this.logRepository.saveLog(log)
-            this.failCallBack(" "+ error)
+            this.failCallBack &&  this.failCallBack(" "+ error)
         }       return false;
     } 
 }
