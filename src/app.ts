16f7fc821c1caeab4bc2826b07894fe934d19afb
Daniel galvan <danielricardogt@gmail.com>
2025-07-14 21:25:16 -0500

    :tada:  mongo db part II

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,32 @@
 import { Server } from "./presentation/server";
 import { envs } from "../src/config/plugins/env.plugins";
+import { LogModel, MongoDataBase } from "./data/mongo";
 
 
 (async()=>{
      main()
 })();
 
-function main() {
-    Server.start();
+async function main(){
+    await MongoDataBase.connect({mongoUrl : envs.MONGO_URL, dbName : envs.MONGO_DB_NAME});
+    
+    
+    //  Server.start();
     // console.log(envs.PORT)
+    
+    // create a collection
+    
+    // const newLog = await LogModel.create({
+    //     message: 'test message form mongo',
+    //     origin: 'App.ts',
+    //     level: 'low', 
+    // })
+
+    // await newLog.save();
+
+    // console.log(newLog)
+    
+     const logs = await LogModel.find();
+     console.log(logs[0].message);
 }
+      
--- src/config/plugins/env.plugins.ts
+++ src/config/plugins/env.plugins.ts
@@ -11,5 +11,5 @@ export const envs = {
     MONGO_URL: env.get('MONGO_URL').required().asString(),
     MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
     MONGO_USER: env.get('MONGO_USER').required().asString(),
-    MONGO_PASSWORD: env.get('MONGO_PASSWORD').required().asString(),
+    MONGO_PASS: env.get('MONGO_PASS').required().asString(),
 }
